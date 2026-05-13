81369c4f4eb9766b3bbeaa0eb3bc641c1ac4b9d9
Jorge Pauvels <jcvels@uvcoding.com.ar>
2026-05-13 16:01:45 -0300

    update: procedures para manejo de alumnos

--- ProyectoIntegrador/Datos/Proyecto.sql
+++ ProyectoIntegrador/Datos/Proyecto.sql
@@ -33,9 +33,33 @@ CREATE INDEX idx_pago_alumno ON Pago(identificador_alumno);
 CREATE INDEX idx_alumno_socio ON Alumno(esSocio);
 
 DELIMITER $
+
 CREATE PROCEDURE Ingresar(in correo varchar(50),in clave varchar(50))
 BEGIN
 	SELECT nombre FROM Usuario u WHERE u.correo = correo AND u.clave = clave;
+END$
+
+CREATE PROCEDURE LeerAlumnos()
+BEGIN
+    SELECT identificador, nombre, apellido, esSocio, vencimiento FROM Alumno;
+END$
+
+CREATE PROCEDURE CrearAlumno(in p_nombre VARCHAR(100), in p_apellido VARCHAR(100), in p_esSocio BOOLEAN, in p_vencimiento DATE)
+BEGIN
+    INSERT INTO Alumno (nombre, apellido, esSocio, vencimiento) VALUES (p_nombre, p_apellido, p_esSocio, p_vencimiento);
+    SELECT LAST_INSERT_ID() AS identificador;
+END$
+
+CREATE PROCEDURE EditarAlumno(in p_identificador INT, in p_nombre VARCHAR(100), in p_apellido VARCHAR(100), in p_esSocio BOOLEAN, in p_vencimiento DATE)
+BEGIN
+    UPDATE Alumno SET nombre = p_nombre, apellido = p_apellido, esSocio = p_esSocio, vencimiento = p_vencimiento WHERE identificador = p_identificador;
+    SELECT ROW_COUNT() AS filas_afectadas;
+END$
+
+CREATE PROCEDURE BorrarAlumno(in p_identificador INT)
+BEGIN
+    DELETE FROM Alumno WHERE identificador = p_identificador;
+    SELECT ROW_COUNT() AS filas_afectadas;
 END$
 
 DELIMITER ;
