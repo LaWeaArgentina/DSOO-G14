84c05061f4923be0e8f71400e6a42c6d6e130837
Jorge Pauvels <jcvels@uvcoding.com.ar>
2026-05-12 22:21:39 -0300

    add: formulario de ingreso

---
+++ ProyectoIntegrador/LogInForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoIntegrador
+{
+    public partial class frmIngresar : Form
+    {
+        public frmIngresar()
+        {
+            InitializeComponent();
+            txtUsuario.Focus();
+        }
+
+        private void borrarDatos()
+        {
+            txtUsuario.Text = string.Empty;
+            txtContrasena.Text = string.Empty;
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            const string USUARIO = "Administrador";
+            const string CONTRASENA = "Admin1234";
+
+            if(txtUsuario.Text == USUARIO & txtContrasena.Text == CONTRASENA)
+            {
+                //this.Hide();
+                Form frmIngreso = new frmIngreso();
+                frmIngreso.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Los datos ingresados no son correctos.\nVerifiquelos y vuelva a intentarlo.",
+                    "Error de ingreso",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Stop
+                );
+                borrarDatos();
+                txtUsuario.Focus();
+            }
+        }
+    }
+}
--- ProyectoIntegrador/Program.cs
+++ ProyectoIntegrador/Program.cs
@@ -16,7 +16,7 @@ namespace ProyectoIntegrador
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmIngresar());
         }
     }
 }
