b614b24e9e02518d46167d4533a2fed7ae37dc34
Jorge Pauvels <jcvels@uvcoding.com.ar>
2026-05-12 21:30:22 -0300

    add: Clase para conexion a mysql

--- ProyectoIntegrador/Datos/Conexion.cs
+++ ProyectoIntegrador/Datos/Conexion.cs
@@ -11,53 +11,47 @@ namespace ProyectoIntegrador.Datos
 {
     public class Conexion
     {
-        // declaramos las variables
         private string baseDatos;
         private string servidor;
         private string puerto;
         private string usuario;
         private string clave;
         private static Conexion? con = null;
-        
+
         private Conexion()
         {
-            this.baseDatos = "Instiruto";
+            this.baseDatos = "Proyecto";
             this.servidor = "localhost";
             this.puerto = "3306";
             this.usuario = "root";
-            this.clave = "";
+            this.clave = "SuperSegura1234";
         }
 
-        // proceso de interacción
         public MySqlConnection CrearConcexion()
         {
-            // instanciamos una conexion
             MySqlConnection? cadena = new MySqlConnection();
-            // el bloque try permite controlar errores
+
             try
             {
-                cadena.ConnectionString = "datasource=" + this.servidor +
-                ";port=" + this.puerto +
-                ";username=" + this.usuario +
-                ";password=" + this.clave +
-                ";Database=" + this.baseDatos;
+                cadena.ConnectionString = "datasource=" + this.servidor + ";port=" + this.puerto + ";username=" + this.usuario + ";password=" + this.clave + ";Database=" + this.baseDatos;
             }
 
-catch (Exception ex)
-{
-cadena = null;
-throw ;
-}
-return cadena;
-}
-// para evaluar la instancia de la conectividad
-public static Conexion getInstancia()
-{
-    if (con == null) // quiere decir que la conexion esta cerrada
-    {
-        con = new Conexion(); // se crea una nueva
+            catch (Exception ex)
+            {
+                cadena = null;
+                throw;
+            }
+
+            return cadena;
+        }
+
+        public static Conexion getInstancia()
+        {
+            if (con == null)
+            {
+                con = new Conexion();
+            }
+            return con;
+        }
     }
-    return con;
-}
-}
 }
