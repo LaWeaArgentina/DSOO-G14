7282731e6ed0983f65c157eef98926e9fa2ca5c1
Jorge Pauvels <jcvels@uvcoding.com.ar>
2026-05-13 12:16:40 -0300

    add: formulario con menu principal de la aplicacion

---
+++ ProyectoIntegrador/frmMenu.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace ProyectoIntegrador
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMenu));
+            this.btnRegistrarAlumno = new System.Windows.Forms.Button();
+            this.btnRegistrarPago = new System.Windows.Forms.Button();
+            this.btnListarVencimientos = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRegistrarAlumno
+            // 
+            this.btnRegistrarAlumno.Location = new System.Drawing.Point(6, 21);
+            this.btnRegistrarAlumno.Name = "btnRegistrarAlumno";
+            this.btnRegistrarAlumno.Size = new System.Drawing.Size(500, 64);
+            this.btnRegistrarAlumno.TabIndex = 0;
+            this.btnRegistrarAlumno.Text = "Registrar Alumno";
+            this.btnRegistrarAlumno.UseVisualStyleBackColor = true;
+            // 
+            // btnRegistrarPago
+            // 
+            this.btnRegistrarPago.Location = new System.Drawing.Point(6, 91);
+            this.btnRegistrarPago.Name = "btnRegistrarPago";
+            this.btnRegistrarPago.Size = new System.Drawing.Size(500, 64);
+            this.btnRegistrarPago.TabIndex = 1;
+            this.btnRegistrarPago.Text = "Registrar Pago";
+            this.btnRegistrarPago.UseVisualStyleBackColor = true;
+            // 
+            // btnListarVencimientos
+            // 
+            this.btnListarVencimientos.Location = new System.Drawing.Point(6, 161);
+            this.btnListarVencimientos.Name = "btnListarVencimientos";
+            this.btnListarVencimientos.Size = new System.Drawing.Size(500, 64);
+            this.btnListarVencimientos.TabIndex = 2;
+            this.btnListarVencimientos.Text = "Listar Vencimientos";
+            this.btnListarVencimientos.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnRegistrarAlumno);
+            this.groupBox1.Controls.Add(this.btnListarVencimientos);
+            this.groupBox1.Controls.Add(this.btnRegistrarPago);
+            this.groupBox1.Location = new System.Drawing.Point(258, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(512, 240);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Menú principal";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(240, 240);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Club Deportivo";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRegistrarAlumno;
+        private System.Windows.Forms.Button btnRegistrarPago;
+        private System.Windows.Forms.Button btnListarVencimientos;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
